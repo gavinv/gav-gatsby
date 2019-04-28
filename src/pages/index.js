af6fed67704f4f4950ca1b7d823831cd02e600e2
gavinv <gavinvaught@gmail.com>
2019-04-28 06:04:48 -0500

    simple test for connecting gatsby to drupal api via GraphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,48 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+export const query = graphql `
+  query allNodeBasePage {
+    allNodeBasePage {
+      edges {
+        node {
+          id
+          title
+          body {
+            value
+            format
+            processed
+            summary
+          }
+        }
+      }
+    }
+  }
+`
+
+
+const IndexPage = ({data}) => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="Gavin Vaught" keywords=
+      {[
+        `gatsby`,
+        `application`,
+        `react`,
+        'gavin vaught',
+        'developer',
+        'drupal',
+        'decoupled'
+      ]}
+    />
+    <div>
+      { data.allNodeBasePage.edges.map(({ node }) => (
+        <div>
+          <h1>{ node.title }</h1>
+          <div dangerouslySetInnerHTML={{ __html: node.body.value }} />
+        </div>
+      ))}
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
