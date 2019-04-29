c3e9b96295354e524a3f61cb4a1909f4c0d19369
gavinv <gavinvaught@gmail.com>
2019-04-29 04:34:46 -0500

    fixed issue importing graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export const query = graphql `
+export const query = { graphql } `
   query allNodeBasePage {
     allNodeBasePage {
       edges {
