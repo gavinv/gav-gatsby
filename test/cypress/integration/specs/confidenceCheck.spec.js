36999a292a84cac7341d7979ff235a9971fbaec9
gavinv <gavinvaught@gmail.com>
2019-05-01 17:29:13 -0500

    converted e2e to cypress

--- test/cypress/integration/specs/confidenceCheck.spec.js
+++ test/cypress/integration/specs/confidenceCheck.spec.js
@@ -1,9 +1,22 @@
-const assert = require('assert');
+/// <reference types="Cypress" />
 
-describe('website', () => {
-  it('should have the right title', () => {
-    browser.url('/');
-    const title = browser.getTitle();
-    assert.equal(title, 'Gavin Vaught');
-  });
-});
+context('Window', () => {
+  beforeEach(() => {
+    cy.visit('https://gavinvaught.com')
+  })
+
+  it('cy.window() - get the global window object', () => {
+    // https://on.cypress.io/window
+    cy.window().should('have.property', 'top')
+  })
+
+  it('cy.document() - get the document object', () => {
+    // https://on.cypress.io/document
+    cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+  })
+
+  it('cy.title() - get the title', () => {
+    // https://on.cypress.io/title
+    cy.title().should('include', 'Gavin Vaught')
+  })
+})
