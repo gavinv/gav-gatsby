ca2e5ae12d1990d84e48bcf0256ab16c83c2536d
gvaught <gavinvaught@gmail.com>
2019-12-12 06:06:12 -0600

    pushing to dev

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,35 +1,36 @@
 import React from "react"
 import styled from "styled-components"
 
-const Btn = styled.button`
+export const BaseButton = styled.button`
+  cursor: pointer;
+  border: none;
+  height: 3.2rem;
+  width: 7.2rem;
   background-color: ${props => (props.bg ? props.bg : "var(--coral)")};
+  border-radius: ${props => (props.rad ? props.rad : "8px 24px 24px 24px")};
   color: ${props => (props.fg ? props.fg : "var(--beige)")};
   font-size: ${props => (props.fontSize ? props.fontSize : "1.2rem")};
-  font-weight: ${props => (props.fontWgt ? props.fontWgt : "364")};
-  filter: ${props =>
-    props.fx ? props.fx : "drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.161))"};
-  border: ${props => (props.border ? props.border : "none")};
-  border-radius: ${props => (props.rad ? props.rad : "8px 24px 24px 24px")};
-  height: ${props => (props.height ? props.height : "4rem")};
-  width: ${props => (props.width ? props.width : "8rem")};
-  cursor: pointer;
-  &:hover {
-    font-weight: 640;
+  font-weight: 480;
+  filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.161));
+  :hover,
+  :focus {
+    font-weight: 720;
     filter: brightness(112%);
-    font-variation-settings: 
+    font-variation-settings:
       "CASL" 1,
       "slnt" -1.6,
       "ital" 0;
   }
   transition: all 256ms ease;
+  ${(props) => props.disabled && `opacity: 0.25; cursor: not-allowed;`}
 `
 
 export default function Button(props) {
   return (
     <>
-      <Btn name={props.label} {...props}>
+      <BaseButton name={props.label} {...props}>
         {props.label}
-      </Btn>
+      </BaseButton>
     </>
   )
 }
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -12,7 +12,7 @@ const FormPanel = styled.div`
   font-variation-settings: "CASL" 0, "ital" 1;
   a {
     font-weight: 320;
-    font-size: 1.32rem;
+    font-size: 1.2rem;
     transition: 250ms;
     text-decoration: underline dotted 1px; 
     &:hover {
@@ -21,22 +21,41 @@ const FormPanel = styled.div`
     }
   }
   .contact-form {
-    display: grid;
-    grid-template: 'f l' 1fr 'e p' 1fr 'm m' 2fr 'r r' .5fr 'x s' .5fr / 1fr 1fr;
-    grid-column-gap: .4em;
+    
+  }
+`
+
+const ButtonWrapper = styled.div`
+  grid-area: s;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
+const Cancel = styled(Button)`
+  font-weight: 256;
+  font-size: 3.2rem;
+  width: 3.2rem;
+  line-height: .8rem;
+  :hover {
+    font-weight: 564;
+  }
+`
+
+const ContactForm = styled.form`
+  display: grid;
+    grid-template: 'f l' 1fr 'e p' 1fr 'm m' 2fr 'r s' .5fr / 1fr 1fr;
+    grid-column-gap: 1.6rem;
     label {
       font-size: 12px;
-      margin: 0 auto;
-      width: 80%;
+      margin: .8rem auto;
+      width: 100%;
       &[for='firstname'] {grid-area: f;}
       &[for='lastname'] {grid-area: l;}
       &[for='email'] {grid-area: e;}
       &[for='phone'] {grid-area: p;}
       &[for='message'] {grid-area: m;}
     }
-    .reset {grid-area: r; width: 24%;}
-    .cancel {justify-self: end;}
-    .submit {justify-self: end;}
+    .reset {grid-area: r;}
     input:-internal-autofill-selected {
       background-color: var(--beige) !important;
     }
@@ -47,7 +66,8 @@ const FormPanel = styled.div`
       background-color: transparent;
       border: 1px solid var(--coral);
       display: grid;
-      padding: .8rem 0;
+      padding: .8rem;
+      margin-top: .32rem;
       width: 100%;
     }
     input {
@@ -58,36 +78,28 @@ const FormPanel = styled.div`
     textarea {
       border-radius: 16px 16px 4px 16px;
     }
-  }
-`
-
-const ButtonWrapper = styled.div`
-  grid-area: s;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
 `
 
 export default function Contact() {
   return (
     <>
-    <SEO title='Contact' />
+    <SEO title='Contact'/>
       <ErrorBoundary>
         <Layout>
         <section className='main blurred'>
           <FormPanel>
-            <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr'}}>
+            <p style={{display: 'grid', gridTemplateColumns: '1fr 1fr'}}>
               <span>
-                <p>call/text:</p>
+                <div>call/text:</div>
                 <a href='tel:+12108441520'>+1 210.844.1520</a>
               </span>
               <span>
-                <p>email:</p>
+                <div>email:</div>
                 <a href='mailto:gavinvaught@gmail.com'>gavinvaught@gmail.com</a>
               </span>
-            </div>
+            </p>
             <p>...or drop me a message from right here:</p>
-            <form
+            <ContactForm
               className='contact-form' 
               name='contact' 
               method="post" 
@@ -119,23 +131,20 @@ export default function Contact() {
               </label>
                 <Button className='reset' type="reset" value="reset" 
                   height='3.2rem'
-                  label='reset'
+                  label='Reset'
                 />
               <ButtonWrapper>
-                <Button className='cancel' type="cancel" value='cancel'
-                  height='3.2rem'
-                  width='3.2rem'
-                  label='x'
+                <Cancel className='cancel' type="cancel" value='cancel'
+                  label='&times;'
                   rad='24px 8px 24px 24px'
                 />
                 <Button 
                   htmlFor='contact'
                   className='submit' type="submit" 
-                  height='3.2rem' 
-                  label='submit'
+                  label='Submit'
                 />
               </ButtonWrapper>
-            </form>
+            </ContactForm>
           </FormPanel>
           </section>
         </Layout>
