d3e3a7c195f3f8a45ae4c2dd2e38ef6d2fbb9000
gvaught <gavinvaught@gmail.com>
2019-12-15 17:40:29 -0600

    added netlify attr to contact form. along with a success page

---
+++ src/pages/success.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import ErrorBoundary from './../components/ErrorBoundary'
+import Layout from './../components/Layout'
+import SEO from './../components/SEO'
+import Link from './../components/Link'
+
+export default function Success() {
+  return (
+    <>
+      <SEO title="Success" />
+      <ErrorBoundary>
+        <Layout>
+          <div>
+          <h1>Form submitted successfully!</h1>
+          <p>Return <Link to='/'>home</Link>...</p>
+          </div>
+        </Layout>
+      </ErrorBoundary>
+    </>
+  )
+}
