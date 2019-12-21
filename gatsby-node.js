1b8086dc0b6e972a5e316fb1dff8a13c1f10a214
gvaught <gavinvaught@gmail.com>
2019-12-21 02:25:07 -0600

    fixes issue with ci artifact path

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,4 @@
 // ./gatsby-node.js
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
-
 exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
   if (page.path.match(/^\/account/)) {
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 import styled from 'styled-components'
 
 import ErrorBoundary from './../components/ErrorBoundary'
@@ -9,8 +9,9 @@ import Input from './../components/Inputs/Input'
 
 const FormPanel = styled.div`
   max-width: 80vw;
-  display: inline-grid;
-  font-variation-settings: "CASL" 0, "ital" 1;
+  display: block;
+  margin: 2rem;
+  font-variation-settings: 'CASL' 0, 'ital' 1;
   a {
     font-weight: 320;
     font-size: 1.2rem;
@@ -18,11 +19,10 @@ const FormPanel = styled.div`
     text-decoration: underline dotted 1px;
     &:hover {
       font-weight: 640;
-      font-variation-settings: "CASL" 1 "ital" 1;
+      font-variation-settings: 'CASL' 1 'ital' 1;
     }
   }
   .contact-form {
-
   }
 `
 
@@ -34,7 +34,7 @@ const ButtonWrapper = styled.div`
 `
 const Cancel = styled(Button)`
   font-weight: 256;
-  font-size: 3.2rem;
+  font-size: 2rem;
   width: 3.2rem;
   line-height: 0;
   :hover {
@@ -44,114 +44,209 @@ const Cancel = styled(Button)`
 
 const ContactForm = styled.form`
   display: grid;
-    grid-template: 'f l' 1fr 'e p' 1fr 'm m' 2fr 'r s' .5fr / 1fr 1fr;
-    grid-column-gap: 1.6rem;
-    label {
-      font-size: 12px;
-      margin: .8rem auto;
-      width: 100%;
-      &[for='firstname'] {grid-area: f;}
-      &[for='lastname'] {grid-area: l;}
-      &[for='email'] {grid-area: e;}
-      &[for='phone'] {grid-area: p;}
-      &[for='message'] {grid-area: m;}
+  grid-template: 'f l' 1fr 'e p' 1fr 'm m' 2fr 'r s' 0.5fr / 1fr 1fr;
+  grid-column-gap: 1.6rem;
+  label {
+    font-size: 12px;
+    margin: 0.8rem auto;
+    width: 100%;
+    &[for='firstname'] {
+      grid-area: f;
     }
-    .reset {grid-area: r;}
-    input:-internal-autofill-selected {
-      background-color: var(--beige) !important;
+    &[for='lastname'] {
+      grid-area: l;
     }
-    input,
-    textarea {
-      font-size: 16px;
-      color: var(--coral);
-      background-color: transparent;
-      border: 1px solid var(--coral);
-      display: grid;
-      padding: .8rem;
-      margin-top: .32rem;
-      width: 100%;
+    &[for='email'] {
+      grid-area: e;
     }
-    input {
-      border: none;
-      border-bottom: 1px solid var(--coral);
-      border-radius: none;
+    &[for='phone'] {
+      grid-area: p;
     }
-    textarea {
-      border-radius: 16px 16px 4px 16px;
+    &[for='message'] {
+      grid-area: m;
     }
+  }
+  .reset {
+    grid-area: r;
+  }
+  input:-internal-autofill-selected {
+    background-color: var(--beige) !important;
+  }
+  input,
+  textarea {
+    font-size: 16px;
+    color: var(--coral);
+    background-color: transparent;
+    border: 1px solid var(--coral);
+    display: grid;
+    padding: 0.8rem;
+    margin-top: 0.32rem;
+    width: 100%;
+  }
+  input {
+    border: none;
+    border-bottom: 1px solid var(--coral);
+    border-radius: none;
+  }
+  textarea {
+    border-radius: 16px 16px 4px 16px;
+  }
 `
 
-export default function Contact() {
-  return (
-    <>
-    <SEO title='Contact'/>
-      <ErrorBoundary>
-        <Layout>
-        <section className='main blurred'>
-          <FormPanel>
-            <p style={{display: 'grid', gridTemplateColumns: '1fr 1fr'}}>
-              <span>
-                <div>call/text:</div>
-                <a href='tel:+12108441520'>+1 210.844.1520</a>
-              </span>
-              <span>
-                <div>email:</div>
-                <a href='mailto:gavinvaught@gmail.com'>gavinvaught@gmail.com</a>
-              </span>
-            </p>
-            <p>...or drop me a message from right here:</p>
-            <ContactForm
-              className='contact-form'
-              name='contact'
-              method="post"
-              action="/success"
-              data-netlify="true" data-netlify-honeypot="bot-field"
-            >
-              <input type="hidden" name="form-name" value="contact" />
-              <label htmlFor='firstname'>
-                First name
-                <Input type="text" name="firstname" className='input--text' />
-              </label>
-              <label htmlFor='lastname'>
-                Last name
-                <Input type="text" name="lastname" className='input--text' />
-              </label>
-              <label htmlFor='email'>
-                Email
-                <Input type="email" name="email" className='input--email' />
-              </label>
-              <label htmlFor='phone'>
-                Phone
-                <Input type="phone" disabled name="phone" className='input--phone' />
-              </label>
-              <label htmlFor='message'>
-                Message
-                <textarea
-                  name="message"
-                  rows="5"
-                  className='input--textarea'
-                />
-              </label>
-                <Button className='reset' type="reset" value="reset"
-                  height='3.2rem'
-                  label='Reset'
-                />
-              <ButtonWrapper>
-                <Cancel className='cancel' type="cancel" value='cancel'
-                  label='&times;'
-                  rad='24px 8px 24px 24px'
-                />
-                <Button
-                  htmlFor='contact'
-                  className='submit' type="submit"
-                  label='Submit'
-                />
-              </ButtonWrapper>
-            </ContactForm>
-          </FormPanel>
-          </section>
-        </Layout>
-      </ErrorBoundary>
-    </>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dirty: false,
+      valid: false,
+      values: {},
+    }
+    this.inputRef = React.createRef()
+  }
+
+  componentDidMount() {
+    this.setState({
+      values: {
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        message: ''
+      }
+    })
+  }
+
+  handleChange = e => {
+    const target = e.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+
+    this.setState(prevState => ({
+      values: {
+        ...prevState.values,
+        [name]: value,
+      },
+    }))
+  }
+
+  cancelSubmission = e => {
+    e.preventDefault()
+    window.location.pathname = '/'
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  render() {
+    return (
+      <>
+        <SEO title='Contact' />
+        <ErrorBoundary>
+          <Layout>
+            <section className='main blurred'>
+              <FormPanel>
+                <p style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+                  <span>
+                    <p>call/text:</p>
+                    <a href='tel:+12108441520'>+1 210.844.1520</a>
+                  </span>
+                  <span>
+                    <p>email:</p>
+                    <a href='mailto:gavinvaught@gmail.com'>
+                      gavinvaught@gmail.com
+                    </a>
+                  </span>
+                </p>
+                <p>...or drop me a message from right here:</p>
+                <ContactForm
+                  className='contact-form'
+                  name='contact'
+                  method='post'
+                  action='/success'
+                  data-netlify='true'
+                  data-netlify-honeypot='bot-field'
+                >
+                  <input type='hidden' name='form-name' value='contact' />
+                  <label htmlFor='firstname'>
+                    First name
+                    <Input
+                      value={this.state.values.firstname}
+                      onChange={e => this.handleChange(e)}
+                      type='text'
+                      name='firstname'
+                      className='input--text'
+                    />
+                  </label>
+                  <label htmlFor='lastname'>
+                    Last name
+                    <Input
+                      value={this.state.values.lastname}
+                      onChange={e => this.handleChange(e)}
+                      type='text'
+                      name='lastname'
+                      className='input--text'
+                    />
+                  </label>
+                  <label htmlFor='email'>
+                    Email
+                    <Input
+                      value={this.state.values.email}
+                      onChange={e => this.handleChange(e)}
+                      type='email'
+                      name='email'
+                      className='input--email'
+                    />
+                  </label>
+                  <label htmlFor='phone'>
+                    Phone
+                    <Input
+                      value={this.state.values.phone}
+                      onChange={e => this.handleChange(e)}
+                      type='phone'
+                      name='phone'
+                      className='input--phone'
+                    />
+                  </label>
+                  <label htmlFor='message'>
+                    Message
+                    <textarea
+                      value={this.state.values.message}
+                      onChange={e => this.handleChange(e)}
+                      rows='5'
+                      name='message'
+                      className='input--textarea'
+                    />
+                  </label>
+                  <Button
+                    className='reset'
+                    type='reset'
+                    height='3.2rem'
+                    label='Reset'
+                  />
+                  <ButtonWrapper>
+                    <Cancel
+                      className='cancel'
+                      type='cancel'
+                      label='&times;'
+                      rad='24px 8px 24px 24px'
+                      onClick={this.cancelSubmission}
+                    />
+                    <Button
+                      htmlFor='contact'
+                      className='submit'
+                      type='submit'
+                      label='Submit'
+                    />
+                  </ButtonWrapper>
+                </ContactForm>
+              </FormPanel>
+            </section>
+          </Layout>
+        </ErrorBoundary>
+      </>
     )
   }
+}
+
+export default Contact
